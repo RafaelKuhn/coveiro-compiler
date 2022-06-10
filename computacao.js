41a3b92f002ad26e2ec6e6b0f917315bffb835ff
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2022-06-09 23:10:24 -0300

    💻 Computaçao marota na terceira pagina

--- computacao.js
+++ computacao.js
@@ -3,15 +3,7 @@ console.log("temo na pagina do compiuter")
 const params = new URLSearchParams(decodeURI(window.location.search))
 
 // const rawMachine = params.get("m").replace(/'/g, "")
-// const rawProgram = params.get("p").replace(/'/g, "")
-
-//  TODO: get json code from url
-// const dataWithLiteralLBs = rawProgram.replace(/<EOL>/g, "\n")
-
-// document.getElementById("codezao").textContent = `código:\n\n${dataWithLiteralLBs}`
-
-// MÁQUINA
-const machine = {
+const templateMaquina = {
 	"registers": 4,
 	"stores": [
 		"a", "b"
@@ -30,104 +22,168 @@ const machine = {
 	],
 }
 
+const rawProgram = params.get("p").replace(/'/g, "")
+const programObj = JSON.parse(rawProgram);
 
-// MONOLÍTICO
-const program = {
-	"lines": 16,
-	"expressions": [
-		null,
-		{ // 2
-			"type": "if",
-			"condition": "a_zero",
-			"onTrue": 5,
-			"onFalse": 0
-		},
-		null, null,
-		{ // 5
-			"type": "if",
-			"condition": "b_zero",
-			"onTrue": 11,
-			"onFalse": 6
-		},
-		{ // 6
-			"type": "call",
-			"what": "c_add",
-			"then": 7
-		},
-		{ // 7
-			"type": "call",
-			"what": "d_add",
-			"then": 8
-		},
-		{ // 8
-			"type": "call",
-			"what": "b_sub",
-			"then": 5
-		},
-		null, null,
-		{ // 11
-			"type": "if",
-			"condition": "c_zero",
-			"onTrue": 16,
-			"onFalse": 12
-		},
-		{ // 12
-			"type": "call",
-			"what": "b_add",
-			"then": 13
-		},
-		{ // 13
-			"type": "call",
-			"what": "c_sub",
-			"then": 11
-		},
-		null, null,
-		{ // 16
-			"type": "call",
-			"what": "a_sub",
-			"then": 2
-		},
-	]
-}
+// document.getElementById("codezao").textContent = `código:\n\n${dataWithLiteralLBs}`
 
+const lineAmount = programObj.lines;
+const expressions = programObj.expressions;
+console.log(programObj.expressions);
 
-document.getElementById("codezao").textContent = `código:\n\n${JSON.stringify(program)}`
-
-const entrada = criaEntradasDoUser()
-
+const entradasNoHTML = criaEntradasDoUser();
 
 
 // isso é chamado pelo botão
 function computaEMostra() {
-	const pre = criaOuAchaPre();
+	const pre = criaOuAchaOPre();
 	
-	const valoresDeEntrada = [];
-	for (let ch_registrador in entrada) {
-		valoresDeEntrada[ch_registrador] = parseInt(entrada[ch_registrador].value);
-	}
-	
-	pre.textContent = simulaComputacao(valoresDeEntrada);
+	const valoresDeEntrada = inicializaRegistradores();
+
+	simulaComputacao(valoresDeEntrada, pre);
 }
 
-function simulaComputacao(entrada) {
-	let a = "usando input:\n"
+/**
+ * @returns {Map<String,Int>}
+ */
+function inicializaRegistradores() {
+	const map = [];
 
-	for (let ch_registrador in entrada) {
-		a += `${ch_registrador}: ${entrada[ch_registrador]}\n`;
+	let caractere = 'a'
+	for(let i = 0; i < templateMaquina.registers; i++) {
+		// console.log("criando valor para o registrador " + caractere);
+		map[caractere] = 0;
+		caractere = incrementaCaractere(caractere);
 	}
 
-	a += "\nTODO: computar!"
+	for (let ch_registrador in entradasNoHTML) {
+		map[ch_registrador] = parseInt(entradasNoHTML[ch_registrador].value);
+	}
 
-	return a;
+	return map;
+}
+
+/**
+ * @param {Map<String, Int>} mapaDeRegistradoresQueArmazenam 
+ * @param {HTMLPreElement} elementoParaDisplay 
+ */
+function simulaComputacao(mapaDeRegistradoresQueArmazenam, elementoParaDisplay) {
+	let linhaAtual = 2; // TODO: acha a linha que o algoritmo começa de fato, não é sempre 2
+	let stringEstadoDosRegistradores = converteIteracaoParaString(linhaAtual, mapaDeRegistradoresQueArmazenam);
+	let stringOutputDaOperacao       = "instrução inicial e valores de entrada armazenados"
+
+	let itCount = 0;
+
+	mostraLinhaDeSaidaNoDisplay(stringOutputDaOperacao, stringEstadoDosRegistradores, elementoParaDisplay);
+
+	while (linhaAtual != 0) {
+		const lineExpression = expressions[linhaAtual - 1];
+		if (!lineExpression) continue; // se for null, a linha é um comentário
+
+		if (lineExpression.type === "if") {
+			console.log(`fazendo um if: ${lineExpression.condition} ? va_para ${lineExpression.onTrue} : va_para ${lineExpression.onFalse}`);
+			
+			if (checaCondicao(lineExpression.condition, mapaDeRegistradoresQueArmazenam)) {
+				linhaAtual = lineExpression.onTrue;
+			} else {
+				console.log("deu false");
+				linhaAtual = lineExpression.onFalse;
+			}
+
+		}
+
+		else if (lineExpression.type === "call") {
+			console.log(`fazendo um call: ${lineExpression.what} -> va_para ${lineExpression.then} `);
+			executaOperacao(lineExpression.what, mapaDeRegistradoresQueArmazenam);
+			linhaAtual = lineExpression.then;
+		}
+	}
+
+	console.log("redirecionado para a linha zero, acabou!");
+}
+
+/**
+ * 
+ * @param {Map<String, Int>} registradores 
+ * @param {String} condicao 
+ * @returns {Boolean}
+ * 
+ */
+function checaCondicao(condicao, registradores) {
+	if (condicao.includes("zero")) {
+		const keyRegistrador = condicao.charAt(0);
+		console.log(`registrador ${keyRegistrador} !! valor ${registradores[keyRegistrador]} `);
+		return registradores[keyRegistrador] == 0;
+	}	
+	else {
+		console.warn("!!!!!!!!!!!!!! CONDICAO NAO INCLUI ZERO !!!!!!!!!!!!!")
+		return false;
+	}
+}
+
+/**
+ * @param {String} operacao 
+ */
+function executaOperacao(operacao, registradores) {
+	const keyRegistrador = operacao.charAt(0);
+	if (operacao.includes("add")) {
+		registradores[keyRegistrador]++;
+	}
+	else if (operacao.includes("sub")) {
+		registradores[keyRegistrador]--;
+	}
+	else {
+		console.warn(" ERRROOOOO OPERAÇAO INVALIDA!! !! !  " + operacao);
+	}
+
+	console.log("valores ate entao: "+converteIteracaoParaString(-1, registradores));
+}
+
+// #############################################################################################################################
+// ###################################### abaixo somente boilerplate de HTML e javascript ######################################
+// #############################################################################################################################
+/**
+ * 
+ * @param {String} caractere 
+ * @returns {String}
+ */
+function incrementaCaractere(caractere) {
+	return String.fromCharCode(caractere.charCodeAt(0) + 1);
+}
+
+/**
+ * @param {Int} numeroDaLinha 
+ * @param {Map<String, Int>} mapaDeRegistradores 
+ * @returns {String}
+ */
+function converteIteracaoParaString(numeroDaLinha, mapaDeRegistradores) {
+	let output = `(${numeroDaLinha}, (`;
+	
+	for (let ch_registrador in mapaDeRegistradores) {
+		// console.log(`reg ${ch_registrador}: ${mapaDeRegistradores[ch_registrador]} `);
+		output += `${ch_registrador}: ${mapaDeRegistradores[ch_registrador]}, `;
+	}
+	output = output.slice(0, -2); // remove os dois últimos caracteres -> ", "
+	output += "))";
+
+	return output;
+}
+
+/**
+ * @param {String} estadoRegistradores 
+ * @param {String} resultadoOperacao 
+ * @param {HTMLPreElement} elementoParaDisplay 
+ */
+function mostraLinhaDeSaidaNoDisplay(resultadoOperacao, estadoRegistradores, elementoParaDisplay) {
+	elementoParaDisplay.textContent += `${resultadoOperacao} -> ${estadoRegistradores}\n`
 }
 
 
-
-// abaixo somente boilerplate de HTML
+/** @returns {Array<inputEl>} */
 function criaEntradasDoUser() {
 	const entradas = []
 	const inputersDiv = document.getElementById("inputers");
-	const registradoreDeEntrada = machine.stores;
+	const registradoreDeEntrada = templateMaquina.stores;
 	
 	registradoreDeEntrada.forEach(caractereDoRegistrador => {
 		const previewEl = document.createElement("span");
@@ -137,6 +193,7 @@ function criaEntradasDoUser() {
 		inputEl.max = 255;
 		inputEl.min = 0;
 		inputEl.value = 0;
+		inputEl.value = 3; // TODO: remove
 		
 		entradas[caractereDoRegistrador] = inputEl;
 		
@@ -148,9 +205,10 @@ function criaEntradasDoUser() {
 }
 
 /** @returns {HTMLPreElement} */
-function criaOuAchaPre() {
+function criaOuAchaOPre() {
 	const preCriado = document.getElementById("runtime");
 	if (preCriado) {
+		preCriado.textContent = "";
 		return preCriado;
 	}
 
@@ -161,7 +219,7 @@ function criaOuAchaPre() {
 	
 	const newPreEl = document.createElement("pre");
 	newPreEl.id = "runtime";
-	newPreEl.textContent = "<>"
+	// newPreEl.textContent = "<>"
 	
 	runtimeContainer.appendChild(newTitleEl);
 	runtimeContainer.appendChild(newPreEl);
